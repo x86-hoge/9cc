e03fe266a9e81bdcf9102443648ffe4c004c0a76
T.Kuriyama <takumigames79@gmail.com>
2019-03-10 14:22:07 +0900

    トークナイズ導入

--- 9cc.c
+++ 9cc.c
@@ -1,31 +1,93 @@
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
+//トークンの型を表す値
+enum{
+	TK_NUM = 256,
+	TK_EOF
+};
+
+//トークン型定義
+typedef struct {
+	int ty;
+	int val;
+	char *input;
+}Token;
+
+Token tokens[100];
+
+void tokenize(char *p){
+	int i = 0;
+	while(*p){
+
+		if(isspace(*p)){
+			p++;
+			continue;
+		}
+
+		if(*p == '+' || *p == '-'){
+			tokens[i].ty = *p;
+			tokens[i].input = p;
+			i++;
+			p++;
+			continue;
+		}
+
+		if(isdigit(*p)){
+			tokens[i].ty = TK_NUM;
+			tokens[i].input = p;
+			tokens[i].val = strtol(p,&p,10);
+			i++;
+			continue;
+		}
+
+		fprintf(stderr,"トークナイズできません: %s\n",p);
+		exit(1);
+	}
+
+	tokens[i].ty = TK_EOF;
+	tokens[i].input = p;
+}
+
+void error(int i){
+	fprintf(stderr,"予期しないトークンです: %s\n",tokens[i].input);
+	exit(1);
+}
 int main(int argc,char **argv){
-	if(argc !=2){
-		fprintf(stderr,"引数の個数が正しくありません\n");
+	if(argc != 2){
+		fprintf(stderr,"引数の個数が正しくありません");
 		return 1;
 	}
-	char *p = argv[1];
+	tokenize(argv[1]);
 	printf(".intel_syntax noprefix\n");
 	printf(".global main\n");
 	printf("main:\n");
-	printf("	mov rax, %ld\n",strtol(p,&p,10));
+
+	if(tokens[0].ty != TK_NUM)error(0);
+	printf("	mov rax, %d\n",tokens[0].val);
+
+	int i = 1;
+	while(tokens[i].ty != TK_EOF){
+		if(tokens[i].ty == '+'){
+			i++;
+			if(tokens[i].ty != TK_NUM)error(i);
+			printf("	add rax,%d\n",tokens[i].val);
+			i++;
+			continue;
+		}
+		if(tokens[i].ty == '-'){
+			i++;	
+			if(tokens[i].ty != TK_NUM)error(1);
+			printf("	sub rax, %d\n",tokens[i].val);
+			i++;
+			continue;
+		}
 	
-	while(*p){
-		if(*p == '+'){
-			p++;
-			printf("	add rax, %ld\n",strtol(p,&p,10));
-			continue;
-		}
-		if(*p == '-'){
-			p++;
-			printf("	sub rax, %ld\n",strtol(p,&p,10));
-			continue;
-		}
-		fprintf(stderr,"予期しない文字です: '%c'\n",*p);
-		return 1;
+		error(i);
 	}
+
 	printf("	ret\n");
 	return 0;
 }
